f6d8b75b03bc56d474546c44564d3caef177dc86
Austin Welch <austinwelch@austins-macbook-pro.local>
2019-02-17 08:56:15 -0500

    Initial Commit

---
+++ Hacker News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hacker News
+//
+//  Created by Austin Welch on 2/17/19.
+//  Copyright © 2019 Austin Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
